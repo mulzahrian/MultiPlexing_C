fed4a275db6b20db83b2453c074b661ceedfe134
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-07-11 13:56:16 +0700

    Add files via upload
    add multipexing

---
+++ multiplexingclient.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT 1972
+#define MAXDATASIZE 100
+
+int c;
+
+int main(int argc, char *argv[])
+{
+    int sockfd, numbytes;
+    char buf[MAXDATASIZE];
+    struct hostent *he;
+    struct sockaddr_in their_addr;
+
+    if(argc !=2) {
+        fprintf(stderr, "usage: client hostname\n");
+        exit(1);
+    }
+    if ((he=gethostbyname(argv[1])) == NULL) {
+        perror("gethostbyname");
+        exit(1);
+    }
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) ==1){
+        perror("socket");
+        exit(1);
+    }
+    their_addr.sin_family = AF_INET;
+    their_addr.sin_port = htons(PORT);
+    their_addr.sin_addr = *((struct in_addr *) he->h_addr);
+    bzero(&(their_addr.sin_zero), 8);
+
+    if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1){
+        perror("connect");
+        exit(1);
+    }
+    int i =0;
+    while(1){
+        if(send(sockfd, "dari client 1\n", 20, 0)== -1){
+            perror("send");
+            exit(1);
+        }
+        printf("after the send function\n");
+
+        if ((numbytes=recv(sockfd, buf, MAXDATASIZE,0)) == -1){
+            perror("recv");
+            exit(1);
+        }
+        buf[numbytes] = '\0';
+
+        printf("received in pid =%d, text=: %s \n", getpid(), buf);
+        sleep(1);
+        i++;
+        break;
+    }
+
+    return 0;
+    }
+
+
